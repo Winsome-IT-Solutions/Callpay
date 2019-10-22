ff444be924dfcab30f22581aa5616a92f919645c
Ravik Ganguly <theravikganguly@gmail.com>
2019-10-22 18:07:01 +0530

    started adding footer content

--- js/script.js
+++ js/script.js
@@ -16,3 +16,14 @@
     });
   });
 })(jQuery);
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    const scroll = $(window).scrollTop();
+    if (scroll > $("#header").height()) {
+      $("#header").addClass("sticky");
+    } else {
+      $("#header").removeClass("sticky");
+    }
+  });
+});
